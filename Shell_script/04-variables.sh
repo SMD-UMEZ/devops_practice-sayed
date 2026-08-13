780a3623ef16567e5b8aea0393c2a476a2247d0f
SMD-UMEZ <sumez180@gmail.com>
2026-08-13 09:41:28 +0530

    variables

--- Shell_script/04-variables.sh
+++ Shell_script/04-variables.sh
@@ -2,7 +2,7 @@
 
 echo "enter the user name :"
 read USER_NAME
-echo " user name is $USER_NAME"
+echo "user name is $USER_NAME"
 echo "enter the password :"
 read -s PASSWORD
 echo "password is $PASSWORD"
