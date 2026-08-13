32d7acb26e6437abc1a94398f0a856c9b190081b
SMD-UMEZ <sumez180@gmail.com>
2026-08-13 09:52:20 +0530

    variables

--- Shell_script/05-variables.sh
+++ Shell_script/05-variables.sh
@@ -1,5 +1,15 @@
 #!/in/bash
 
-TIME_STAMP=$(date)
+#TIME_STAMP=$(date)
 
-echo " Time is :$TIME_STAMP"
+#echo " Time is :$TIME_STAMP"
+
+START_TIME=$(date +%s)
+
+sleep =5
+
+END_TIME=$(date +%s)
+
+total_time=$(($END_TIME-$START_TIME))
+
+echo " total time to execute this cmd:$total_time"
