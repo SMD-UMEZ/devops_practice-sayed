d67fb933a54d299d4775370d85de644168969c30
SMD-UMEZ <sumez180@gmail.com>
2026-08-14 15:11:46 +0530

    special_vars

--- Shell_script/08-conditions.sh
+++ Shell_script/08-conditions.sh
@@ -10,6 +10,6 @@ if [ $NUMBER -gt 20 ]; then
 echo " given Number is greater than 20"
 elif [ $NUMBER -eq 20 ]
 echo " given Number is equal to 20"
-else 
+else
 echo " Given number is less than 20 "
 fi
