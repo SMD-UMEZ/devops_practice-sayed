c643770b52303f68e3be5f5125425ab8d77111f1
SMD-UMEZ <sumez180@gmail.com>
2026-08-14 11:39:46 +0530

    special_vars

--- Shell_script/07-datatypes.sh
+++ Shell_script/07-datatypes.sh
@@ -6,3 +6,8 @@ NUM2=$2
 SUM=$(($NUM1+$NUM2))
 
 echo "total count:$SUM"
+
+### array #####
+MOVIES=("RRR" "SPIDER" "AVENGERS")
+echo "all movie names: ${MOVIES[@]}"
+echo "third movie name: ${MOVIES[3]}"
