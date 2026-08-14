badf788c703617d0eb83ea5b7bc614ab011a6e73
SMD-UMEZ <sumez180@gmail.com>
2026-08-14 17:12:43 +0530

    special_vars

--- Shell_script/09-install.sh
+++ Shell_script/09-install.sh
@@ -6,3 +6,13 @@ if [  $USER -ne 0 ]; then
 echo "please run this script with root access"
 exit 1
 fi
+
+echo "installing my sql"
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then 
+echo "installing failure"
+exit 1
+else
+echo "success"
+fi
