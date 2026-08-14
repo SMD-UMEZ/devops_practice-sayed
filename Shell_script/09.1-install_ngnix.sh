5370fc75254185de32e5a713ee1ba28f7d39cd25
SMD-UMEZ <sumez180@gmail.com>
2026-08-14 17:39:40 +0530

    special_vars

---
+++ Shell_script/09.1-install_ngnix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+echo "please login with root access to run this script"
+exit 1
+fi
+
+dnf install ngnix -y
+
+if [ $? -ne 0 ]; then 
+echo " installation failed"
+exit 1
+else 
+echo " installation success"
+fi
