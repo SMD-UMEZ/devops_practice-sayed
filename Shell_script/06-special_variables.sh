aab1a0bef41c80f5340af69d7fa7f0ff8d78712d
SMD-UMEZ <sumez180@gmail.com>
2026-08-14 10:49:17 +0530

    special_var

--- Shell_script/06-special_variables.sh
+++ Shell_script/06-special_variables.sh
@@ -6,5 +6,6 @@ echo "All variables passed to script: $@"
 echo "Number of variables passed : $#"
 echo "First variable: $1"
 echo " Script name : $0"
-echo " who is running the script : $user"
+echo " who is running the script : $USER "
 echo "hi"
+USER=sayed
