e4b43b957bd96347bdf07f8e39becb8888806ceb
SMD-UMEZ <sumez180@gmail.com>
2026-08-16 09:49:33 +0530

    special_vars

--- Shell_script/09.1-install_ngnix.sh
+++ Shell_script/09.1-install_ngnix.sh
@@ -9,10 +9,28 @@ fi
 
 dnf install pythddon -y
 status=$($?)
-echo " installing python......"
+
 if [ $status -ne 0 ]; then
 echo " python installation failed"
 exit 1
 else
 echo " python installation success"
+fi
+
+
+USER=$( id -u)
+
+if [  $USER -ne 0 ]; then
+echo "please run this script with root access"
+exit 1
+fi
+
+echo "installing my sql"
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then 
+echo "installing failure"
+exit 1
+else
+echo "success"
 fi
