384388978c8f8d283d4b211496595024cb4cef68
SMD-UMEZ <sumez180@gmail.com>
2026-08-16 09:35:09 +0530

    special_vars

--- Shell_script/09.1-install_ngnix.sh
+++ Shell_script/09.1-install_ngnix.sh
@@ -6,3 +6,13 @@ if [ $USER -ne 0 ]; then
 echo "please login as root user"
 exit 1
 fi
+
+dnf install pythddon -y
+
+echo " installing python......"
+
+if [ $? -ne 0 ]; then
+echo " python installation failed"
+else
+echo " python installation success"
+fi
