d5742ce2a814df4ad8f535c7f6e0bba832e661fa
SMD-UMEZ <sumez180@gmail.com>
2026-08-17 11:24:51 +0530

    special_vars

--- Shell_script/09.1-install_ngnix.sh
+++ Shell_script/09.1-install_ngnix.sh
@@ -15,5 +15,25 @@ if [ $? -ne 0 ];then
 echo "program is not installed"
 exit 1
 else
-echo "program is installed to successfully"
+echo "program is installed  successfully"
 fi
+
+### Functions    #########
+
+Valid(){
+    if [ $2 -ne 0 ]; then
+    echo "programm is not installed"
+    exit 1
+    else
+    echo "program is installed"
+    fi
+}
+
+dnf install python3 gcc python3-devel -y
+Valid python $?
+
+dnf install golang -y
+Valid golang $?
+
+dnf install jaaaav -y
+Valid java $?
