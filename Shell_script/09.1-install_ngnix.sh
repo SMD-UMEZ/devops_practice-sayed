97fb941f56a37dab59ab1244d1f21553f9782278
SMD-UMEZ <sumez180@gmail.com>
2026-08-17 10:25:52 +0530

    special_vars

--- Shell_script/09.1-install_ngnix.sh
+++ Shell_script/09.1-install_ngnix.sh
@@ -7,3 +7,11 @@ echo "Installing mongodb............"
 else
 echo "login as root user to proceed..."
 fi
+
+dnf install gogfgdb -y
+
+if [ $? -ne 0 ];then
+echo "program is not installed"
+else
+echo "program is installed to successfully"
+fi
