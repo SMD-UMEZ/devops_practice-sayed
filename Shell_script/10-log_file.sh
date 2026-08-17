925030e147102c46c541e370c5cf68df06e706a1
SMD-UMEZ <sumez180@gmail.com>
2026-08-17 14:07:59 +0530

    log_script

--- Shell_script/10-log_file.sh
+++ Shell_script/10-log_file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 user=$(id -u)
-All_logs=/var/log/shell-logs
-log_files= "$All_logs/$0.log"  ## /home/ec2-user/shell-logs/filename.log##
+All_logs=/home/ec2-user/shell-logs/
+log_files="$All_logs/$0.log"  ## /home/ec2-user/shell-logs/filename.log##
 if [ $user -ne 0 ]; then
 echo "please login as Root user"
 exit 1
